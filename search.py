659d63b71ec49b854317da94e1479e0d37c3c2d6
Lisa Xu <50823583+ZeroxAlone@users.noreply.github.com>
2019-06-30 10:10:56 +0800

    Add files via upload

---
+++ search.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 30 07:59:02 2019
+
+@author: translucky
+"""
+
+import time
+
+book_words=file_handle=open('alice_in_wonderland.txt',mode='r')
+#("alice_in_wonderland.txt")
+bigger_vocab=file_handle=open('vocab.txt',mode='r')
+
+def find_unknown_words(vocab, wds):
+    """ Return a list of words in wds that do not occur in vocab """
+    result = []
+    for w in wds:
+        if (search_linear(vocab, w) < 0):
+            result.append(w)
+    return result
+
+def search_linear(xs, target):
+    """ Find and return the index of target in sequence xs """
+    for (i, v) in enumerate(xs):
+       if v == target:
+           return i
+    return -1
+
+t0 = time.clock()
+missing_words = find_unknown_words(bigger_vocab, book_words)
+t1 = time.clock()
+print("There are {0} unknown words.".format(len(missing_words)))
+print("That took {0:.4f} seconds.".format(t1-t0))
+ 
